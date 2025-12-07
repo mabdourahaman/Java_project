5070e62e15b731feb99be8e2191633fafa907716
Youssouf <you.moussa@edu.umi.ac.ma>
2025-12-07 15:03:33 +0100

    Update App.java

--- gestion-biblio/src/main/java/ma/est/gestion/App.java
+++ gestion-biblio/src/main/java/ma/est/gestion/App.java
@@ -1,5 +1,31 @@
 package ma.est.gestion;
 
-public class App {
+import ma.est.gestion.controller.LivreController;
+import ma.est.gestion.dao.impl.LivreDaoImpl;
+import ma.est.gestion.view.LivrePanel;
 
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        // DAO
+        LivreDaoImpl dao = new LivreDaoImpl();
+
+        // Controller créé d'abord (sans vue)
+        LivreController controller = new LivreController(null, dao);
+
+        // Vue créée ensuite (elle prend le controller)
+        LivrePanel view = new LivrePanel(controller);
+
+        // On lie la vue au controller
+        controller.setView(view);
+
+        // Affichage
+        JFrame frame = new JFrame("Gestion des Livres");
+        frame.setContentPane(view);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
 }
