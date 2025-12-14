7042df76796f8739718b2e83c80f600b5e17fe26
Dametote Jean BOGOU <j.bogoudametote@edu.umi.ac.ma>
2025-12-14 15:03:05 +0100

    Update Adherent.java
    Voici le code de la classe Model de la classe Adhérent que j'ai codé

--- gestion-biblio/src/main/java/ma/est/gestion/model/Adherent.java
+++ gestion-biblio/src/main/java/ma/est/gestion/model/Adherent.java
@@ -1,40 +1,34 @@
 package ma.est.gestion.model;
 
 public class Adherent {
-
-
     private final int NumAdherent;
-    private final String NomAdherent;
-    private final String PrenomAdherent;
-    private final String EmailAdherent;
+    private  String NomAdherent;
+    private  String PrenomAdherent;
+    private  String EmailAdherent ;
 
-
-    public Adherent(int NumAdherent, String NomAdherent, String PrenomAdherent, String EmailAdherent) {
-        this.NumAdherent = NumAdherent;
-        this.NomAdherent = NomAdherent;
-        this.PrenomAdherent = PrenomAdherent;
+    public Adherent( int  NumAdherent, String EmailAdherent, String NomAdherent, String PrenomAdherent){
         this.EmailAdherent = EmailAdherent;
+        this.NumAdherent = NumAdherent ;
+        this.NomAdherent = NomAdherent ;
+        this.PrenomAdherent = PrenomAdherent ;
+
+    }
+        public int getNumAdherent() {
+        return NumAdherent;
     }
 
-    public int getNumAdherent() { return NumAdherent;}
+    public String getNomAdherent() {
+        return NomAdherent;
+    }
 
-    public String getNomAdherent() { return NomAdherent; }
+    public String getPrenomAdherent() {
+        return PrenomAdherent;
+    }
 
-    public String getPrenomAdherent() { return PrenomAdherent; }
+    public String getEmailAdherent() {
+        return EmailAdherent;
+    }
     
-    public String getEmailAdherent() { return EmailAdherent; }
-
-    public String getFullName() {
-        return this.PrenomAdherent + " " + this.NomAdherent;
-    }
-
-    @Override
-    public String toString() {
-        return "Adherent{" +
-                "NumAdherent=" + NumAdherent +
-                ", NomAdherent='" + NomAdherent + '\'' +
-                ", PrenomAdherent='" + PrenomAdherent + '\'' +
-                ", EmailAdherent='" + EmailAdherent + '\'' +
-                '}';
-    }
 }
+
+
