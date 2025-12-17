cd48a9cf0f2129710a1f4b001bf634fa3e28e182
mabdourahaman <m.abdourahaman@edu.umi.ac.ma>
2025-12-17 21:34:45 +0100

    Update EmpruntController.java

--- gestion-biblio/src/main/java/ma/est/gestion/controller/EmpruntController.java
+++ gestion-biblio/src/main/java/ma/est/gestion/controller/EmpruntController.java
@@ -1,6 +1,14 @@
 package ma.est.gestion.controller;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
 import ma.est.gestion.dao.EmpruntDao;
+import ma.est.gestion.model.Emprunt;
 import ma.est.gestion.view.EmpruntPanel;
 
 /**
@@ -9,6 +17,7 @@ import ma.est.gestion.view.EmpruntPanel;
  */
 public class EmpruntController {
 
+    
     private EmpruntDao dao;
     private EmpruntPanel view;
 
@@ -17,18 +26,98 @@ public class EmpruntController {
         this.view = view;
 
         view.refreshTable(dao.getAllEmprunts());
+
+
+        initController();
+    }
+
+    private void initController() {
+
         view.getSupprimer().addActionListener(e -> supprimerEmprunt());
         view.getModifier().addActionListener(e -> modifierEmprunt());
 
     }
 
-    public void supprimerEmprunt(){
+    public void supprimerEmprunt() {
 
+        int Row = view.getTable().getSelectedRow();
+
+        if (Row == -1) {
+
+            JOptionPane.showMessageDialog( view, "Veuillez sélectionner un emprunt à supprimer",
+            "Information", JOptionPane.INFORMATION_MESSAGE
+            );
+            return;
+        }
+
+        String codeEmprunt = view.getTable().getValueAt(Row, 0).toString();
+
+        int confirm = JOptionPane.showConfirmDialog( view, "Voulez-vous supprimer l'emprunt " + codeEmprunt + " ?",
+        "Confirmation", JOptionPane.YES_NO_OPTION   );
+
+        if (confirm != JOptionPane.YES_OPTION) {
+            return;
+        }
+
+        var emprunt = dao.findEmpruntByCode(codeEmprunt);
+
+        dao.deleteEmprunt(emprunt);
+        view.refreshTable(dao.getAllEmprunts());
     }
 
+
+    @SuppressWarnings("CallToPrintStackTrace")
     public void modifierEmprunt(){
 
+        int Row = view.getTable().getSelectedRow();
+
+        if (Row == -1) {
+
+            JOptionPane.showMessageDialog( view, "Veuillez sélectionner un emprunt à supprimer",
+            "Information", JOptionPane.INFORMATION_MESSAGE
+            );
+            return;
+        }
+        
+        String codeEmprunt = view.getTable().getValueAt(Row, 0).toString();
+        int numAdherent = Integer.parseInt(view.getTable().getValueAt(Row, 1).toString());
+
+        String dateEmp = view.getTable().getValueAt(Row, 2).toString();
+        String dateRet = view.getTable().getValueAt(Row, 3).toString();
+        
+        SimpleDateFormat sdf;
+        if (dateEmp.length() == 10) {
+            sdf = new SimpleDateFormat("yyyy-MM-dd");
+        } else {
+            sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+        Date dateEmprunt = null;
+        Date dateRetour = null; 
+
+        try {
+            dateEmprunt = sdf.parse(dateEmp); 
+            dateRetour = sdf.parse(dateRet); 
+            
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String statut = view.getTable().getValueAt(Row, 4).toString();
+        String codeLivre = view.getTable().getValueAt(Row, 5).toString();
+
+        int confirm = JOptionPane.showConfirmDialog( view, "Voulez-vous modifier l'emprunt " + codeEmprunt + " ?",
+        "Confirmation", JOptionPane.YES_NO_OPTION   );
+
+        if (confirm != JOptionPane.YES_OPTION) {
+            view.refreshTable(dao.getAllEmprunts());
+            return;
+        }
+
+        Emprunt emprunt = new Emprunt( codeEmprunt, numAdherent, dateEmprunt, dateRetour, statut, codeLivre);
+
+        dao.modifierEmprunt(emprunt);
+        JOptionPane.showMessageDialog(view, "Emprunt modifié avec succès !");
+        view.refreshTable(dao.getAllEmprunts());
+
     }
-    
 
 }
