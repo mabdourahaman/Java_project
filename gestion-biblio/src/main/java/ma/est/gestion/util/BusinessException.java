cfbd53e0a53497868767e894d407e0fbf6cdde2e
Youssouf <you.moussa@edu.umi.ac.ma>
2025-12-19 23:07:16 +0100

    Update BusinessException.java

--- gestion-biblio/src/main/java/ma/est/gestion/util/BusinessException.java
+++ gestion-biblio/src/main/java/ma/est/gestion/util/BusinessException.java
@@ -1,5 +1,19 @@
 package ma.est.gestion.util;
 
-public class BusinessException {
+//Exception personnalisée utilisée pour signaler les erreurs liées
+ //à la logique métier (business rules) de l'application.
 
+public class BusinessException extends RuntimeException {
+
+    // Identifiant de sérialisation pour la compatibilité
+    private static final long serialVersionUID = 1L;
+
+  
+    public BusinessException(String message) {
+        super(message);
+    }
+    
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
