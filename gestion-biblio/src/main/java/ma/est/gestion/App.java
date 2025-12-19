9393fe314324a504446915c9ef674ceb046accd1
Youssouf <you.moussa@edu.umi.ac.ma>
2025-12-19 23:18:50 +0100

    Update App.java

--- gestion-biblio/src/main/java/ma/est/gestion/App.java
+++ gestion-biblio/src/main/java/ma/est/gestion/App.java
@@ -1,55 +1,16 @@
 package ma.est.gestion;
 
 import javax.swing.SwingUtilities;
-
-import ma.est.gestion.controller.EmpruntController;
-import ma.est.gestion.dao.impl.EmpruntDaoImpl;
-import ma.est.gestion.view.EmpruntPanel;
+import ma.est.gestion.view.LoginFrame;
 
 public class App {
 
-
     public static void main(String[] args) {
 
-        // Exécution de la calsse Adherent panel
-
-       /* 
-        AdherentPanel view = new AdherentPanel();
-        AdherentDao dao = new AdherentDao();
-
-        new AdherentController(dao, view);
-        view.setVisible(true);
-*/
-
-        
-        
-        EmpruntDaoImpl dao = new EmpruntDaoImpl();
-        EmpruntPanel view = new EmpruntPanel();
-        new EmpruntController(dao, view);
-        view.setVisible(true); 
-             
-        SwingUtilities.invokeLater(() -> new App());
-        /*
-        LivreDaoImpl dao = new LivreDaoImpl();
-
-        // Controller créé d'abord (sans vue)
-        LivreController controller = new LivreController(null, dao);
-
-        // Vue créée ensuite (elle prend le controller)
-        LivrePanel view = new LivrePanel(controller);
-
-        // On lie la vue au controller
-        controller.setView(view);
-
-        // Affichage
-        JFrame frame = new JFrame("Gestion de Bibliothèque Universitaire");
-        frame.setContentPane(view);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-    }
-         */
-        // DAO
-
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame login = new LoginFrame();
+            login.setLocationRelativeTo(null);
+            login.setVisible(true);
+        });
     }
 }
