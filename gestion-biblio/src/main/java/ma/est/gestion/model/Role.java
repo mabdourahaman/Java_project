ecb72e11daaf899f5cc7415a7e72669263dfbde1
Youssouf <you.moussa@edu.umi.ac.ma>
2025-12-19 23:11:56 +0100

    Update Role.java

--- gestion-biblio/src/main/java/ma/est/gestion/model/Role.java
+++ gestion-biblio/src/main/java/ma/est/gestion/model/Role.java
@@ -1,5 +1,15 @@
 package ma.est.gestion.model;
 
 public class Role {
+    private String nomRole; // ex : "ADMIN", "ADHERENT"
 
+    public Role(String nomRole) {
+        this.nomRole = nomRole;
+    }
+
+    public String getNomRole() { return nomRole; }
+    public void setNomRole(String nomRole) { this.nomRole = nomRole; }
+
+    @Override
+    public String toString() { return nomRole; }
 }
