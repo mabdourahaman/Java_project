2572cf9ec3323eb6472fe033e0dc4b6326facbb0
Youssouf <you.moussa@edu.umi.ac.ma>
2025-12-19 23:10:56 +0100

    Refactor Categorie class methods
    Updated getter method name and added toString method.

--- gestion-biblio/src/main/java/ma/est/gestion/model/Categorie.java
+++ gestion-biblio/src/main/java/ma/est/gestion/model/Categorie.java
@@ -1,14 +1,19 @@
 package ma.est.gestion.model;
 
 public class Categorie {
-	private String nomCategorie;
-	
-	public Categorie(String nomCategorie){
-		this.nomCategorie = nomCategorie;
-	}
-	
-	public String getCategorie() {return nomCategorie;}
-	public void setCategorie(String nomCategorie) {
-		this.nomCategorie = nomCategorie;
-	}
+
+    private String nomCategorie;
+
+    public Categorie(String nomCategorie) {
+        this.nomCategorie = nomCategorie;
+    }
+
+    public String getNomCategorie() {
+        return nomCategorie;
+    }
+
+    @Override
+    public String toString() {
+        return nomCategorie;
+    }
 }
