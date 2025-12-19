22138a0b7fe4930ade20755e975ecb0b06cec5e7
Youssouf <you.moussa@edu.umi.ac.ma>
2025-12-19 23:11:28 +0100

    Enhance Log class with action details
    Added fields and constructor to Log class for action logging.

--- gestion-biblio/src/main/java/ma/est/gestion/model/Log.java
+++ gestion-biblio/src/main/java/ma/est/gestion/model/Log.java
@@ -1,5 +1,24 @@
 package ma.est.gestion.model;
 
-public class Log {
+import java.time.LocalDateTime;
 
+public class Log {
+    private LocalDateTime dateAction;
+    private String action;
+    private Utilisateur utilisateur;
+
+    public Log(LocalDateTime dateAction, String action, Utilisateur utilisateur) {
+        this.dateAction = dateAction;
+        this.action = action;
+        this.utilisateur = utilisateur;
+    }
+
+    // Getters / Setters
+    public LocalDateTime getDateAction() { return dateAction; }
+    public String getAction() { return action; }
+    public Utilisateur getUtilisateur() { return utilisateur; }
+
+    public void setDateAction(LocalDateTime dateAction) { this.dateAction = dateAction; }
+    public void setAction(String action) { this.action = action; }
+    public void setUtilisateur(Utilisateur utilisateur) { this.utilisateur = utilisateur; }
 }
