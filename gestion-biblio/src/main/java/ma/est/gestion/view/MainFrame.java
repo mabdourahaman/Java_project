54d435916a3c7a65b3ebc52e54eb495d09125518
Youssouf <you.moussa@edu.umi.ac.ma>
2025-12-19 23:16:56 +0100

    Refactor MainFrame to include menu and actions

--- gestion-biblio/src/main/java/ma/est/gestion/view/MainFrame.java
+++ gestion-biblio/src/main/java/ma/est/gestion/view/MainFrame.java
@@ -1,5 +1,67 @@
 package ma.est.gestion.view;
 
-public class MainFrame {
+import java.awt.*;
+import javax.swing.*;
 
+import ma.est.gestion.controller.LivreController;
+import ma.est.gestion.dao.LivreDao;
+
+public class MainFrame extends JFrame {
+
+    private JPanel contentPanel;
+
+    public MainFrame() {
+
+        setTitle("Gestion Bibliothèque - Admin");
+        setSize(1100, 650);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        // MENU BAR 
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu menuGestion = new JMenu("Gestion");
+        JMenuItem itemLivres = new JMenuItem("Livres");
+        JMenuItem itemAdherents = new JMenuItem("Adhérents");
+        JMenuItem itemQuitter = new JMenuItem("Quitter");
+
+        menuGestion.add(itemLivres);
+        menuGestion.add(itemAdherents);
+        menuGestion.addSeparator();
+        menuGestion.add(itemQuitter);
+
+        menuBar.add(menuGestion);
+        setJMenuBar(menuBar);
+
+        //PANEL CENTRAL
+        contentPanel = new JPanel(new BorderLayout());
+        add(contentPanel, BorderLayout.CENTER);
+
+        //ACTIONS MENU
+        itemLivres.addActionListener(e -> afficherLivres());
+        itemAdherents.addActionListener(e ->
+                JOptionPane.showMessageDialog(this,
+                        "Interface adhérent"));
+
+        itemQuitter.addActionListener(e -> System.exit(0));
+
+        //AFFICHAGE PAR DÉFAUT
+        afficherLivres();
+    }
+
+    private void afficherLivres() {
+
+        contentPanel.removeAll();
+
+        LivrePanel livrePanel = new LivrePanel();
+        LivreDao dao = new LivreDao();
+        new LivreController(livrePanel, dao);
+
+        contentPanel.add(livrePanel.getContentPane(),
+                BorderLayout.CENTER);
+
+        contentPanel.revalidate();
+        contentPanel.repaint();
+    }
 }
+
